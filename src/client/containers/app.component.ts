37971d70ccd97c6946ee881153fad0235b0490ee
graftss <coughcoughcough@gmail.com>
2016-08-05 23:36:53 -0400

    (refactor) use `ConfigureStoreService` in `App` component

--- src/client/containers/app.component.ts
+++ src/client/containers/app.component.ts
@@ -9,13 +9,14 @@ import { Login } from '../components/login.component';
 import { TeacherDashboard } from '../components/teacherDashboard.component';
 import { MenuBarComponent } from '../components/menuBar.component';
 import { IAppState, rootReducer, enhancers } from '../store/index';
+import { ConfigureStoreService } from '../services/configure-store.service.ts';
 const createLogger = require('redux-logger');
 
 @Component({
   selector: 'app',
   directives: [ SearchBar, StudentDashboard, MenuBarComponent, Login, AdvancedSearch, ROUTER_DIRECTIVES ],
   pipes: [ AsyncPipe ],
-  providers: [ DevToolsExtension ],
+  providers: [ DevToolsExtension, ConfigureStoreService ],
   template: `
   <h3>Here is the home page</h3>
   <h4>Welcome to taco tuts</h4>
@@ -29,14 +30,9 @@ export class App {
   constructor(
     public router: Router,
     private ngRedux: NgRedux<IAppState>,
-    private devTool: DevToolsExtension) {
-      console.log('store')
-    // Do this once in the top-level app component.
-    this.ngRedux.configureStore(
-      rootReducer,
-      {},
-      // [],
-      [ createLogger() ],
-      [ ...enhancers, devTool.isEnabled() ? devTool.enhancer() : f => f]);
+    private devTool: DevToolsExtension,
+    private configureStore: ConfigureStoreService
+  ) {
+    configureStore.configure(ngRedux);
   }
 }
