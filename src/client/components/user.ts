6c256581bdcafc73e17165571224b4b70eb83df1
Emily Jones <efhjones@gmail.com>
2016-08-07 19:50:08 -0600

    (feat) conform user model to database model

--- src/client/components/user.ts
+++ src/client/components/user.ts
@@ -1,10 +1,11 @@
 export class User {
   constructor(
-    public id: number,
     public username: string,
-    public password: string,
-    public subjects: string,
-    public teacher: boolean,
-    public student: boolean
+    public email: string,
+    public student: any,
+    public learningSubjects: any,
+    public teacher: any,
+    public teachingSubjects: any
+
   ) {  }
 }
