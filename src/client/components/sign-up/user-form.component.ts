10e0ca34214221a76beab656a9daf5cdac43ac79
Emily Jones <efhjones@gmail.com>
2016-08-09 15:00:55 -0600

    (feat) use auth0 to sign up users (feat) create user in database upon successful sign up through auth0 (chore) update instance of new User to conform to model (chore) remove diagnostic

--- src/client/components/sign-up/user-form.component.ts
+++ src/client/components/sign-up/user-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NgForm }    from '@angular/common';
 import { User }    from './user';
 import { HTTP_PROVIDERS } from '@angular/http';
@@ -7,55 +7,45 @@ import { IAppState } from '../../store';
 import { Router,
          ActivatedRouteSnapshot,
          RouterStateSnapshot }    from '@angular/router';
+import { Auth } from '../../services/auth.service';
 
 @Component({
   selector: 'user-form',
   providers: [ NewUserService ],
-  template: require('./user-form.component.html')  
+  template: require('./user-form.component.html'),
+  directives: []
 })
 export class UserFormComponent {
 
   constructor(
     private newUserService: NewUserService,
-    private router: Router
+    private router: Router,
+    private auth: Auth
   ) { }
 
-  isStudentSelected: boolean = false;
-
-  showStudentSubjects() {
-    this.isStudentSelected = !this.isStudentSelected;
-    if (this.isStudentSelected === false) {
-      this.model.learningSubjects = ''; 
-    }
-  }
-
-  isTeacherSelected: boolean = false;
-
-  showTeacherSubjects() {
-    this.isTeacherSelected = !this.isTeacherSelected;
-    if (this.isTeacherSelected === false) {
-      this.model.teachingSubjects = ''; 
-    }
-  }
-  
   create(dataObj) {
     this.newUserService.createUser(dataObj)
     .subscribe(
       response => {
-        console.log(response)
+        console.log(response);
+        this.router.navigate(['/student-dash'])
+
       },
       error => console.log('error', error)
     );
-    //this.router.navigate(['/student-dash']);
   }
- 
+
+  signUp(model) {
+    this.auth.signUp(model, (response) => {
+      this.create(model);
+
+    });
+  }
+
   onSubmit() { this.submitted = true; }
 
-  subjects = ['Chemistry', 'Math', 'Javascript'];
-  model = new User('', '', '', '', '', false, '');
+  model = new User('', '', '');
   submitted = false;
-  
-  // TODO: Remove this when we're done
-  get diagnostic() { return JSON.stringify(this.model); }
-}
+  validPasswords = true;
 
+}
