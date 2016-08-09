875fbd412f95b7f0f23527db6010718ea19a841a
Emily Jones <efhjones@gmail.com>
2016-08-09 16:37:02 -0600

    (chore) linting tasks         -remove unused components         -rename components to containt `Component`

--- src/client/components/app.routes.ts
+++ src/client/components/app.routes.ts
@@ -1,24 +1,19 @@
 import { RouterConfig, provideRouter } from '@angular/router';
-import { MenuBarComponent } from './menuBar.component'
-import { AdvancedSearch } from './advancedSearch.component';
-import { StudentDashboard } from './studentDashboard.component';
-import { Login } from './login.component';
+import { AdvancedSearchComponent } from './advancedSearch.component';
+import { StudentDashboardComponent } from './studentDashboard.component';
+import { LoginComponent } from './login.component';
 import { SignUpComponent } from './sign-up/signUp.component';
-import { TeacherDashboard } from './teacherDashboard.component';
-import { App } from '../containers/app.component';
-
-
+import { TeacherDashboardComponent } from './teacherDashboard.component';
 
 const routes: RouterConfig = [
-  { path: 'login',  component: Login },
+  { path: 'login',  component: LoginComponent },
   { path: 'sign-up',  component: SignUpComponent },
-  { path: 'student-dash', component: StudentDashboard },
-  { path: 'teacher-dash', component: TeacherDashboard },
-  { path: 'advanced-search', component: AdvancedSearch },
+  { path: 'student-dash', component: StudentDashboardComponent },
+  { path: 'teacher-dash', component: TeacherDashboardComponent },
+  { path: 'advanced-search', component: AdvancedSearchComponent },
   { path: '**', redirectTo: '/student-dash' }
 
 ];
-
 
 export const appRouterProviders = [
     provideRouter(routes)
