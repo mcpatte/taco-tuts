add83ec40e38c10fdcbe5af37ee72954d839b3c3
Emily Jones <efhjones@gmail.com>
2016-08-09 14:37:35 -0600

    (chore) remove favorite color entry from login component         change location of `Login with Google` button to be next to log in (feat)  add Router Directives to Directives (feat)  add link to navigate to separate sign up page

--- src/client/components/login.component.ts
+++ src/client/components/login.component.ts
@@ -3,10 +3,20 @@ import { Component } from '@angular/core';
 import { NgRedux, select } from 'ng2-redux';
 import { IAppState, rootReducer, enhancers } from '../store/index';
 import { Auth } from '../services/auth.service';
+import { Router,
+         ROUTER_DIRECTIVES,
+         ActivatedRouteSnapshot,
+         RouterStateSnapshot }    from '@angular/router';
+
 
 @Component({
   selector: 'login',
-  providers: [ ],
+  directives: [ ROUTER_DIRECTIVES ],
+  styles: [`
+    a {
+      color: white;
+    }
+  `],
   template: `
     <div>
         <div class="row">
@@ -20,27 +30,28 @@ import { Auth } from '../services/auth.service';
                 <label for="name">Password</label>
                 <input type="password" class="form-control" #password placeholder="your password">
               </div>
-              <div class="form-group">
-                <label for="name">Color</label>
-                <input type="text" class="form-control" #color placeholder="favorite color">
-              </div>
               <button type="submit" class="btn btn-default" (click)="auth.login(username.value, password.value)">Login</button>
-              <button type="submit" class="btn btn-default" (click)="auth.signUp(username.value, password.value, color.value)">Sign Up</button>
+               <button type="submit" class="btn btn-default btn-primary" (click)="auth.googleLogin()">Login with google</button>
+              <h4>Don't have an account yet?</h4>
             </form>
-            <button type="submit" class="btn btn-default btn-primary" (click)="auth.googleLogin()">Login with google</button>
+            <button type="submit" class="btn btn-default btn-primary">
+              <a routerLink="/sign-up" routerLinkActive="active">Sign Up</a>
+            </button>
           </div>
         </div>
       </div>
   `
 })
 export class Login {
-  // Selected observables to test async pipe model.
-
-  // Members to test subscribe model.
 
   constructor(
     private auth: Auth,
+    private router: Router,
     private ngRedux: NgRedux<IAppState>
     ) { }
+
+    goToSignup(){
+      this.router.navigate(['/sign-up']);
+    }
 }
 
