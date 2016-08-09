cada666a6c5b76ece856082d0e55de1cef1b3e6d
graftss <coughcoughcough@gmail.com>
2016-08-09 18:48:19 -0400

    (feat) change `baseWhiteboardUrl` to point to a shared awwapp board

--- src/client/components/session-widgets/whiteboard-link.component.ts
+++ src/client/components/session-widgets/whiteboard-link.component.ts
@@ -5,5 +5,8 @@ import { Component } from '@angular/core';
   template: require('./whiteboard-link.template.html')
 })
 export class WhiteboardLinkComponent {
-  public baseWhiteboardUrl: string = 'https://www.awwapp.com';
+  // it seems as though the `/b/*` will automatically redirect to
+  // a new shared whiteboard, whereas from the index page you'd have
+  // to create a shared whiteboard yourself
+  public baseWhiteboardUrl: string = 'https://www.awwapp.com/b/anything';
 }
