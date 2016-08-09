8acb2ab59b394ad759ff6b3ede7e83401bb28ea4
Emily Jones <efhjones@gmail.com>
2016-08-09 16:41:29 -0600

    (chore) linting tasks         -rename `SearchBar` to contain `Component`         -remove unused imports

--- src/client/components/searchBar.component.ts
+++ src/client/components/searchBar.component.ts
@@ -1,22 +1,17 @@
 //This should search for tutors
 //should be on advanced search page for now
 import { Component } from '@angular/core';
-import { NgRedux, select } from 'ng2-redux';
-import { IAppState, rootReducer, enhancers } from '../store/index';
+import { NgRedux } from 'ng2-redux';
+import { IAppState } from '../store/index';
 
 @Component({
   selector: 'search-bar',
   providers: [ ],
   template: `
-  <h3>OMG I AM THE SEARCHBAR!</h3>
-    
+  <h3>OMG I AM THE SEARCHBAR!</h3>  
   `
 })
-export class SearchBar {
-  // Selected observables to test async pipe model.
-  
-  // Members to test subscribe model.
-
+export class SearchBarComponent {
   constructor(
     private ngRedux: NgRedux<IAppState>) { }
 }
