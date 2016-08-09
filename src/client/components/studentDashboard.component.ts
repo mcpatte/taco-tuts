1480999623203e9b014a8c8bf6e7086df64ddd3d
Emily Jones <efhjones@gmail.com>
2016-08-09 16:42:34 -0600

    (chore) linting tasks         -remove comments         -remove unused imports         -remove whitespace         -rename `StudentDashboard` to contain `Component`

--- src/client/components/studentDashboard.component.ts
+++ src/client/components/studentDashboard.component.ts
@@ -1,27 +1,15 @@
-//this will render the search bar
-//needs logo
-//*needs menu bar
-//show goals
-//show calendar
-//*show upcoming sessions
-//show messages
 import { Component } from '@angular/core';
-import { NgRedux, select } from 'ng2-redux';
-import { IAppState, rootReducer, enhancers } from '../store/index';
+import { NgRedux } from 'ng2-redux';
+import { IAppState } from '../store/index';
 import { Auth } from '../services/auth.service';
 
 @Component({
   selector: 'student-dashboard',
   template: `
   <h3>OMG I AM THE STUDENT DASHBOARD!</h3>
-
   `
 })
-export class StudentDashboard {
-  // Selected observables to test async pipe model.
-
-  // Members to test subscribe model.
-
+export class StudentDashboardComponent {
   constructor(
     private auth: Auth,
     private ngRedux: NgRedux<IAppState>) { }
