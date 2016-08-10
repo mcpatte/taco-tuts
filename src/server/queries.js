6216330ea0b560412f4bed7cc1c90981954fcb90
graftss <coughcoughcough@gmail.com>
2016-08-10 17:56:10 -0400

    (feat) add server route to set teacher availability

--- src/server/queries.js
+++ src/server/queries.js
@@ -192,6 +192,29 @@ function removeUser(req, res, next){
     });
 };
 
+function setAvailability(req, res, next) {
+  var authID = req.params.authID;
+  var availability = req.body.availability;
+
+  db.result(
+    'update users set isAvailible = $1 where authID = $2',
+    [availability, authID]
+  ).then(function(result) {
+    res.status(200)
+      .json({
+        status: 'success',
+        message: `Updated availability of ${authID} to ${availability}`
+      });
+  })
+  .catch(function(err) {
+    res.status(400)
+      .json({
+        status: 'failure',
+        message: err.message
+      });
+  });
+}
+
 module.exports = {
   getAllUsers: getAllUsers,
   getSingleUser: getSingleUser,
@@ -204,5 +227,6 @@ module.exports = {
   getSubjectForTeacher: getSubjectForTeacher,
   getTeachersForSubject: getTeachersForSubject,
   getTeaching: getTeaching,
-  removeUser: removeUser
+  removeUser: removeUser,
+  setAvailability: setAvailability,
 };
