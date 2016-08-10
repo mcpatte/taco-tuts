7953b4e4c31de4211c9ec06331131bd459096636
Emily Jones <efhjones@gmail.com>
2016-08-09 22:19:17 -0600

    (fix) set let userID to reference correct item from action

--- src/client/store/login.reducer.ts
+++ src/client/store/login.reducer.ts
@@ -11,15 +11,13 @@ const INIT_STATE: ILoginState = {
 export function loginReducer(
   state = INIT_STATE,
   action): ILoginState {
-
-  switch (action.type) {
-    case LOGIN_ACTIONS.SET_USER_ID:
-      console.log("Oh hello, I'm the login reducer. Action.payload: ", action.payload, " state: ", state);
-      let userID = action.payload.userID;
-      return Object.assign({}, state, {
-        userID
-      });
-    default:
-      return state;
-  }
+    switch (action.type) {
+      case LOGIN_ACTIONS.SET_USER_ID:
+        let userID = action.userID;
+        return Object.assign({}, state, {
+          userID
+        });
+      default:
+        return state;
+    }
 }
