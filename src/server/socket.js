7388bbc43aaef0e99ac245f8b49d21336c842ef6
graftss <coughcoughcough@gmail.com>
2016-08-11 02:44:41 -0400

    (refactor) refactor socket handlers out of connection handlers

--- src/server/socket.js
+++ src/server/socket.js
@@ -8,33 +8,37 @@ function initSocket(io) {
     const userID = socket.handshake.query.userID;
     connections = Object.assign(connections, { [userID]: socket });
 
-    socket.on('request-session', function(data) {
-      const { teacherID, student } = data;
-      const teacherSocket = connections[teacherID];
-
-      if (teacherSocket) {
-        teacherSocket.emit('request-session', { student });
-      }
-    });
-
-    socket.on('accept-session', function(data) {
-      const { teacherID, studentID } = data;
-      const sessionID = Math.random().toString(36).substring(4, 9);
-      const teacher = connections[teacherID];
-      const student = connections[studentID];
-
-      const session = new Session(teacher, student, sessionID);
-
-      sessions[sessionID] = session;
-      session.start();
-    });
-
-    socket.on('session-message', function(data) {
-      const { sessionID, message } = data;
-      const session = sessions[sessionID];
-      session.emitMessage(message);
-    });
+    socket.on('request-session', onSessionRequest);
+    socket.on('accept-session', onSessionAccept);
+    socket.on('session-message', onSessionMessage);
   });
+}
+
+function onSessionRequest(data) {
+  const { teacherID, student } = data;
+  const teacherSocket = connections[teacherID];
+
+  if (teacherSocket) {
+    teacherSocket.emit('request-session', { student });
+  }
+}
+
+function onSessionAccept(data) {
+  const { teacherID, studentID } = data;
+  const sessionID = Math.random().toString(36).substring(4, 9);
+  const teacher = connections[teacherID];
+  const student = connections[studentID];
+
+  const session = new Session(teacher, student, sessionID);
+
+  sessions[sessionID] = session;
+  session.start();
+}
+
+function onSessionMessage(data) {
+  const { sessionID, message } = data;
+  const session = sessions[sessionID];
+  session.emitMessage(message);
 }
 
 module.exports = initSocket;
