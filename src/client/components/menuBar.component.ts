a60a3ae7e2c058e4f0df0074ca0daa285ba150d1
Emily Jones <efhjones@gmail.com>
2016-08-11 01:28:15 -0600

    (chore) remove *ngIf statements from menu items so all items show (needs to be fixed later) (feat)  call logout function on logout click to remove authId from state

--- src/client/components/menuBar.component.ts
+++ src/client/components/menuBar.component.ts
@@ -1,30 +1,37 @@
-import { Component } from '@angular/core';
-import { NgRedux } from 'ng2-redux';
-import { IAppState } from '../store/index';
-import { ROUTER_DIRECTIVES } from '@angular/router';
-import { Auth } from '../services/auth.service';
-import { HomeComponent } from '../components/home.component';
+import { Component }          from '@angular/core';
+import { NgRedux }            from 'ng2-redux';
+import { IAppState }          from '../store/index';
+import { ROUTER_DIRECTIVES }  from '@angular/router';
+import { Auth }               from '../services/auth.service';
+import { LogoutActions }      from '../actions/logout.actions';
 
 
 @Component({
   selector: 'menu-bar',
-  directives: [ ROUTER_DIRECTIVES,  ],
+  directives: [ ROUTER_DIRECTIVES ],
+  providers: [ LogoutActions ],
   template: `
   <nav>
     <a routerLink="/home" routerLinkActive="active">Home</a>
-    <a routerLink="/student-dash" routerLinkActive="active" *ngIf="auth.authenticated()">Student Dashboard</a>
-    <a routerLink="/teacher-dash" routerLinkActive="active" >Teacher Dashboard</a>
+    <a routerLink="/student-dash" routerLinkActive="active">Student Dashboard</a>
+    <a routerLink="/teacher-dash" routerLinkActive="active">Teacher Dashboard</a>
     <a routerLink="/advanced-search" routerLinkActive="active">Advanced Search</a>
-    <a routerLink="/login" routerLinkActive="active" *ngIf="!auth.authenticated()">Log In</a>
-    <a routerLink="#" routerLinkActive="active" (click)="auth.logout()" *ngIf="auth.authenticated()">Log Out</a>
+    <a routerLink="/login" routerLinkActive="active">Log In</a>
+    <a routerLink="#" routerLinkActive="active" (click)="logout()">Log Out</a>
   </nav>
   `
 })
 export class MenuBarComponent {
   constructor(
     private auth: Auth,
-    private ngRedux: NgRedux<IAppState>
-    ) { }
-}
+    private ngRedux: NgRedux<IAppState>,
+    private logoutActions: LogoutActions
 
+    ) { }
+
+    logout () {
+      this.auth.logout();
+      this.logoutActions.setLogoutDispatch();
+    }
+}
 //*ngIf="user-service.isTeacher()"
