8dac49f78fa50037864bdf5b94147880fa141559
Emily Jones <efhjones@gmail.com>
2016-08-10 20:33:34 -0600

    (feat) create methods in login actions to save and get user data

--- src/client/actions/login.actions.ts
+++ src/client/actions/login.actions.ts
@@ -1,5 +1,6 @@
 import { NgRedux } from 'ng2-redux';
 import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
 
 export const LOGIN_ACTIONS = {
   SET_USER_ID: 'SET_USER_ID'
@@ -7,12 +8,44 @@ export const LOGIN_ACTIONS = {
 
 @Injectable()
 export class LoginActions {
-  constructor(private ngRedux: NgRedux<any>) {}
 
-  setLoginDispatch(id: string) {
+  private userUrl: string = '/api/users/';
+
+  constructor(
+    private ngRedux: NgRedux<any>,
+    private http: Http
+    ) {}
+
+  setLoginDispatch(authID: string, email: string) {
+    const envelope = {
+      authID: authID,
+      email: email
+    }
+
+    //call local setAuthID function
+    this.setAuthID(authID, envelope)
+
     this.ngRedux.dispatch({
       type: LOGIN_ACTIONS.SET_USER_ID,
-      userID: id
+      userID: authID
     });
+    
+    this.getUserData(authID);
   }
+
+  //Set AuthID in database
+  private setAuthID(authID: string, data: Object) {
+    this.http.post(this.userUrl + authID, data)
+      .subscribe(
+        (response) => console.log(response)
+      )
+  }
+
+  private getUserData(authID: string) {
+     this.http.get(this.userUrl + authID)
+      .subscribe (
+        (response) => console.log(response)
+      )
+  }
+
 }
