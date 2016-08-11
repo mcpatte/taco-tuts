b9497a1d0631eee90ac01b1bf7a80fef64458650
graftss <coughcoughcough@gmail.com>
2016-08-11 01:33:10 -0400

    (feat) render session state in `SessionComponent`

--- src/client/components/session/session.component.ts
+++ src/client/components/session/session.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import { SessionWidgetsComponent } from '../session-widgets/session-widgets.component';
 import { NgRedux, select } from 'ng2-redux';
 import { IAppState } from '../../store';
+import { IMessageState } from '../../store/session.reducer';
 
 @Component({
   selector: 'session',
@@ -9,7 +11,18 @@ import { IAppState } from '../../store';
   template: require('./session.template.html')
 })
 export class SessionComponent {
+  @select(['session', 'role']) role$: Observable<string>;
+  @select(['session', 'messages']) messages$: Observable<IMessageState[]>;
+
   constructor(
     private ngRedux: NgRedux<IAppState>
   ) { }
+
+  isTeacher() {
+    return this.ngRedux.getState().session.role === 'teacher';
+  }
+
+  onKeypress(event) {
+    console.log(event);
+  }
 }
