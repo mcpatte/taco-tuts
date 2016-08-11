1d7aabde83d41326349592585238471aec6b0572
Emily Jones <efhjones@gmail.com>
2016-08-11 01:36:12 -0600

    (feat) add user.service to get user data

---
+++ src/client/services/user.service.ts
@@ -0,0 +1,28 @@
+import { Injectable }     from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+
+@Injectable()
+export class UserService {
+  userUrl : string = '/api/users/';
+  constructor (private http: Http) {}
+
+  public getUserData(authID: string) {
+    console.log('This getUserData function is so hot right now', authID);
+    return this.http.get(this.userUrl + authID)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.data || { };
+  }
+
+  private handleError (error: any) {
+    let errMsg = (error.message) ? error.message :
+    error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+}
