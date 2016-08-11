84db2f73c94859f4fd0c635ba8fef6798f899272
graftss <coughcoughcough@gmail.com>
2016-08-10 23:32:05 -0400

    (feat) add `onRequestedSession` method to `SocketService`
    refactor `listeners` to be created once on connection

--- src/client/services/socket.service.ts
+++ src/client/services/socket.service.ts
@@ -5,25 +5,39 @@ import * as io from 'socket.io-client';
 @Injectable()
 export class SocketService {
   private socket = null;
+  private listeners = {};
+  private events: string[] = [
+    'request-session'
+  ];
 
   constructor() {
     setTimeout(() => {
-      this.requestSession('auth0|57ab5d3a10d863e85429247a')
+      const id = 'auth0|57ab5d3a10d863e85429247a';
+      this.onRequestedSession(data => console.log('woop', data))
+      this.requestSession(id, { name: 'harambe', userID: id });
     }, 1000);
   }
 
   connect(userID) {
     this.socket = io(undefined, { query: `userID=${userID}` });
     this.socket.on('message', data => console.log('socket data', data));
+
+    this.events.forEach((event) => {
+      this.listeners[event] = this.getListener(event);
+    });
   }
 
-  listen(event) {
+  getListener(event) {
     return Observable.create((observer) => {
       this.socket.on(event, data => observer.next(data));
-    })
+    });
   }
 
-  requestSession(teacherID) {
-    this.socket.emit('student-request-session', { teacherID });
+  requestSession(teacherID, student) {
+    this.socket.emit('request-session', { teacherID, student });
+  }
+
+  onRequestedSession(callback) {
+    this.listeners['request-session'].subscribe(callback);
   }
 }
