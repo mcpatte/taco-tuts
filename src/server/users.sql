ffdd0ceeaa7c7ac27c639c7051ce75e13774fb3c
Chase Patterson <mcpatte@gmail.com>
2016-08-11 16:45:45 -0700

    (feat) update file to add user data to remote database

--- src/server/users.sql
+++ src/server/users.sql
@@ -27,3 +27,64 @@ CREATE TABLE teaching (
   userID integer references users(id) ON DELETE CASCADE,
   subjectID integer references subjects(id) ON DELETE CASCADE
 );
+
+INSERT INTO users (email, authid, username, name, teacher, isavailible)
+  VALUES ('deathwatcher@crazy.com', 'auth9', 'crazy person', 'Luna Lovegood', false, false);
+  
+INSERT INTO users (email, authid, username, name, teacher, isavailible)
+  VALUES ('hp@hogwarts.com', 'auth8', 'chosen one', 'Harry Potter', false, false);
+
+INSERT INTO users (email, authid, username, name, teacher, isavailible)
+  VALUES ('hagrid@gmail.com', 'auth7', 'Hagrid', 'Hagrid', true, true);
+
+INSERT INTO users (email, authid, username, name, teacher, isavailible)
+  VALUES ('mmoody', 'auth5', 'crazyeyes', 'Mad-Eye Moody', true, false);
+
+INSERT INTO users (email, authid, username, name, teacher, isavailible)
+  VALUES ('rweasly@yahoo.com', 'auth4', 'Ronald', 'Ron', false, false);
+
+INSERT INTO users (email, authid, username, name, teacher, isavailible)
+  VALUES ('vmor@hotmail.com', 'auth6', 'Tom', 'Voldemort', true, true);
+
+INSERT INTO users (email, authid, username, name, teacher, isavailible)
+  VALUES ('ddoor@gmail.com', 'auth3', 'Headmaster', 'Dumbledoor', true, true);
+
+INSERT INTO users (email, authid, username, name, teacher, isavailible)
+  VALUES ('hbp@hotwarts.com', 'auth1', 'halfbloodprince', 'Severus Snape', true, true);
+
+INSERT INTO subjects (name)
+  VALUES ('Potions');
+
+INSERT INTO subjects (name)
+  VALUES ('Defense against the dark arts'); 
+
+INSERT INTO subjects (name)
+  VALUES ('Magical Creatures');
+
+INSERT INTO subjects (name)
+  VALUES ('Taco Making');
+
+INSERT INTO subjects (name)
+  VALUES ('Dueling');
+
+INSERT INTO teaching (userID, subjectID)
+  VALUES (1, 1);
+
+INSERT INTO teaching (userID, subjectID)
+  VALUES (4, 2);
+
+INSERT INTO teaching (userID, subjectID)
+  VALUES (5, 2);
+
+INSERT INTO teaching (userID, subjectID)
+  VALUES (6, 3);
+
+INSERT INTO teaching (userID, subjectID)
+  VALUES (4, 3);
+
+INSERT INTO teaching (userID, subjectID)
+  VALUES (2, 4);
+
+INSERT INTO teaching (userID, subjectID)
+  VALUES (2, 5);
+
