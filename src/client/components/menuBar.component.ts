a57a5ea495c1d1e027bec6749f1735089aeccafc
Emily Jones <efhjones@gmail.com>
2016-08-10 20:39:55 -0600

    (feat) show student and teacher dashboard conditionally based on authentication

--- src/client/components/menuBar.component.ts
+++ src/client/components/menuBar.component.ts
@@ -5,14 +5,15 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
 import { Auth } from '../services/auth.service';
 import { HomeComponent } from '../components/home.component';
 
+
 @Component({
   selector: 'menu-bar',
-  directives: [ ROUTER_DIRECTIVES ],
+  directives: [ ROUTER_DIRECTIVES,  ],
   template: `
   <nav>
     <a routerLink="/home" routerLinkActive="active">Home</a>
-    <a routerLink="/student-dash" routerLinkActive="active">Student Dashboard</a>
-    <a routerLink="/teacher-dash" routerLinkActive="active">Teacher Dashboard</a>
+    <a routerLink="/student-dash" routerLinkActive="active" *ngIf="auth.authenticated()">Student Dashboard</a>
+    <a routerLink="/teacher-dash" routerLinkActive="active" >Teacher Dashboard</a>
     <a routerLink="/advanced-search" routerLinkActive="active">Advanced Search</a>
     <a routerLink="/login" routerLinkActive="active" *ngIf="!auth.authenticated()">Log In</a>
     <a routerLink="#" routerLinkActive="active" (click)="auth.logout()" *ngIf="auth.authenticated()">Log Out</a>
@@ -25,3 +26,5 @@ export class MenuBarComponent {
     private ngRedux: NgRedux<IAppState>
     ) { }
 }
+
+//*ngIf="user-service.isTeacher()"
