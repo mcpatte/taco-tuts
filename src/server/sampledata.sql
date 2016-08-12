4eb870d17bd270a55bf7773dd7c0cf64c93fa78d
graftss <coughcoughcough@gmail.com>
2016-08-12 19:21:34 -0400

    (feat) add real users to seed data

--- src/server/sampledata.sql
+++ src/server/sampledata.sql
@@ -32,7 +32,7 @@ CREATE TABLE teaching (
 
 INSERT INTO users (email, authid, username, name, teacher, isavailible)
   VALUES ('hbp@hotwarts.com', 'auth1', 'halfbloodprince', 'Severus Snape', true, true);
-  
+
 INSERT INTO users (email, authid, username, name, teacher, isavailible)
   VALUES ('hp@hogwarts.com', 'auth8', 'chosen one', 'Harry Potter', false, false);
 
@@ -54,11 +54,17 @@ INSERT INTO users (email, authid, username, name, teacher, isavailible)
 INSERT INTO users (email, authid, username, name, teacher, isavailible)
   VALUES ('deathwatcher@crazy.com', 'auth9', 'crazy person', 'Luna Lovegood', false, false);
 
+INSERT INTO users (email, authid, username, name, teacher, isavailible)
+  VALUES ('stu@gmail.com', 'auth0|57acab1d645d9d914448d2ee', 'stu', 'stu gene', false, false);
+
+INSERT INTO users (email, authid, username, name, teacher, isavailible)
+  VALUES ('teach@gmail.com', 'auth0|57acae2010d863e8542927e8', 'teach_dude88', 'super teach dude', true, true);
+
 INSERT INTO subjects (name)
   VALUES ('Potions');
 
 INSERT INTO subjects (name)
-  VALUES ('Defense against the dark arts'); 
+  VALUES ('Defense against the dark arts');
 
 INSERT INTO subjects (name)
   VALUES ('Magical Creatures');
@@ -89,3 +95,9 @@ INSERT INTO teaching (userID, subjectID)
 
 INSERT INTO teaching (userID, subjectID)
   VALUES (7, 5);
+
+INSERT INTO teaching (userID, subjectID)
+  VALUES (10, 2);
+
+INSERT INTO teaching (userID, subjectID)
+  VALUES (10, 5);
