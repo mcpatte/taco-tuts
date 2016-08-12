8b779a9c743b901dd1b41a0068d97eb44c5fb18b
graftss <coughcoughcough@gmail.com>
2016-08-12 18:22:48 -0400

    (refactor) refactor socket code to rely on `ConnectionManager`

--- src/server/socket/connection-manager.js
+++ src/server/socket/connection-manager.js
@@ -1,13 +1,49 @@
+var _ = require('lodash');
+var Session = require('./session');
+
 function ConnectionManager() {
   this.connections = {};
+  this.sessions = {};
+
+  _.each(ConnectionManager.prototype, function(method, name) {
+    this[name] = method.bind(this);
+    console.log({ thus: this, method, name })
+  }.bind(this));
 }
 
-ConnectionManager.prototype.add = function(id, socket) {
+ConnectionManager.prototype.addConnection = function(id, socket) {
   this.connections[id] = socket;
 };
 
-ConnectionManager.prototype.get = function(id) {
+ConnectionManager.prototype.getConnection = function(id) {
   return this.connections[id];
+};
+
+ConnectionManager.prototype.onSessionRequest = function(data) {
+  var { teacherID, student } = data;
+  var teacherSocket = this.getConnection(teacherID);
+
+  if (teacherSocket) {
+    teacherSocket.emit('request-session', { student });
+  }
+};
+
+ConnectionManager.prototype.onSessionAccept = function(data) {
+  var { teacherID, studentID } = data;
+  var sessionID = Math.random().toString(36).substring(4, 9);
+  var session = new Session(sessionID);
+
+  this.sessions[sessionID] = session;
+
+  session.addTeacher(this.getConnection(teacherID));
+  session.addStudent(this.getConnection(studentID));
+  session.start();
+};
+
+ConnectionManager.prototype.onSessionMessage = function(data) {
+  var { sessionID, message } = data;
+  var session = this.sessions[sessionID];
+  session.emitMessage(message);
 };
 
 module.exports = ConnectionManager;
--- src/server/socket/socket.js
+++ src/server/socket/socket.js
@@ -1,45 +1,17 @@
 var ConnectionManager = require('./connection-manager');
-var Session = require('./session');
 
 var connectionManager = new ConnectionManager();
-let sessions = {};
 
 function initSocket(io) {
   io.on('connection', function(socket) {
-    const userID = socket.handshake.query.userID;
-    connectionManager.add(userID, socket);
+    var userID = socket.handshake.query.userID;
 
-    socket.on('request-session', onSessionRequest);
-    socket.on('accept-session', onSessionAccept);
-    socket.on('session-message', onSessionMessage);
+    connectionManager.addConnection(userID, socket);
+
+    socket.on('request-session', connectionManager.onSessionRequest);
+    socket.on('accept-session', connectionManager.onSessionAccept);
+    socket.on('session-message', connectionManager.onSessionMessage);
   });
-}
-
-function onSessionRequest(data) {
-  const { teacherID, student } = data;
-  const teacherSocket = connectionManager.get(teacherID);
-
-  if (teacherSocket) {
-    teacherSocket.emit('request-session', { student });
-  }
-}
-
-function onSessionAccept(data) {
-  const { teacherID, studentID } = data;
-  const sessionID = Math.random().toString(36).substring(4, 9);
-  const session = new Session(sessionID);
-
-  sessions[sessionID] = session;
-
-  session.addTeacher(connectionManager.get(teacherID));
-  session.addStudent(connectionManager.get(studentID));
-  session.start();
-}
-
-function onSessionMessage(data) {
-  const { sessionID, message } = data;
-  const session = sessions[sessionID];
-  session.emitMessage(message);
 }
 
 module.exports = initSocket;
