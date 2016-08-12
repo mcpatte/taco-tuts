cc8a940231f774c0b9f1de7d8a5af5d4d64fdda7
kpl39 <kylelinhardt@gmail.com>
2016-08-12 15:18:25 -0400

    (feat) add profile service

---
+++ src/client/services/profile.service.ts
@@ -0,0 +1,14 @@
+import { Injectable }     from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable()
+export class ProfileService {
+  constructor (private http: Http) {}
+  
+  
+  updateUser(userData: Object, authID: string) {
+    return this.http.put('/api/users/' + authID, userData);
+  }
+}
+
+
