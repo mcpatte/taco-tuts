a0b404f225ee81c54aac11dab9465911ebf5ffd5
Emily Jones <efhjones@gmail.com>
2016-08-15 13:11:16 -0600

    (fix) fix typo in teachers schema, organize queries by method

--- src/server/server.js
+++ src/server/server.js
@@ -37,20 +37,17 @@ app.get('/api/teaching/:id', db.getSubjectForTeacher);
 app.get('/api/teaching', db.getTeaching);
 app.get('/api/learning', db.getLearning);
 app.get('/api/subject/:id', db.getTeachersForSubject);
+app.get('/api/learning/:id', db.findSubjectsByUser);
 app.post('/api/users', db.createUser);
 app.post('/api/subject', db.createSubject);
 app.post('/api/learning', db.learningSubject);
 app.post('/api/teaching', db.teachingSubject);
 app.post('/api/users/:authID', db.setAuthID);
-
-
 app.post('/api/teaching/:authID', db.insertTeacher);
-
 app.post('/api/available/:authID', db.setAvailability);
 app.put('/api/users/:authID', db.updateUser);
 app.delete('/api/users/:authID', db.removeUser);
 app.delete('/api/subject/:id', db.removeSubject);
-app.get('/api/learning/:id', db.findSubjectsByUser);
 app.delete('/api/learning/:userID/:subjectID', db.removeSubjectByUser);
 
 
