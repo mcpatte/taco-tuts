63e8e33ff597a2a33eb9d7370fea365801a61ba8
graftss <coughcoughcough@gmail.com>
2016-08-15 23:34:19 -0400

    (refactor) remove unused reducer and state

--- src/client/store/index.ts
+++ src/client/store/index.ts
@@ -1,21 +1,16 @@
 import { combineReducers } from 'redux';
 const persistState = require('redux-localstorage');
-import { ISearchState, searchReducer } from './search.reducer';
 import { ILoginState, loginReducer } from './login.reducer';
 import { ITeacherState, teacherReducer } from './teacher.reducer';
 import { ISessionState, sessionReducer } from './session.reducer';
 
 export interface IAppState {
-  counter?: number;
-  search?: ISearchState;
-  userData?: ILoginState;
   login?: ILoginState;
   teacher?: ITeacherState;
   session?: ISessionState;
 };
 
 export const rootReducer = combineReducers<IAppState>({
-  search: searchReducer,
   login: loginReducer,
   teacher: teacherReducer,
   session: sessionReducer
