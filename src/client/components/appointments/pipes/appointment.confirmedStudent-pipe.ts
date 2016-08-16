797bbec61495316b9ba221a194da69afd49c9ee5
kpl39 <kylelinhardt@gmail.com>
2016-08-16 13:54:29 -0400

    (feat) add filtering pipe for appointments

---
+++ src/client/components/appointments/pipes/appointment.confirmedStudent-pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe } from '@angular/core';
+
+
+@Pipe ({
+    name: "confirmedStudent"
+})
+
+export class ConfirmedStudentPipe {
+    transform(value, target) {
+        return value.filter((item) => item.confirmed === target && item.isteacher === false)
+    }
+} 
