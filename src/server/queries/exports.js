346a0e31d328664ad03ec84c23e261084841c38b
graftss <coughcoughcough@gmail.com>
2016-08-16 16:22:54 -0400

    (refactor) refactor query exports

--- src/server/queries/exports.js
+++ src/server/queries/exports.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 var users = require('./users');
 var subjects = require('./subject');
 var available = require('./available');
@@ -7,32 +9,13 @@ var sessions = require('./sessions');
 var teachers = require('./teachers');
 var advSearch = require('./advanced-search');
 
-module.exports = {
-  getAllUsers: users.getAllUsers,
-  getSingleUser: users.getSingleUser,
-  createUser: users.createUser,
-  updateUser: users.updateUser,
-  removeUser: users.removeUser,
-  setAuthID: users.setAuthID,
-  getTeachersForSubject: subjects.getTeachersForSubject,
-  createSubject: subjects.createSubject,
-  getAllSubjects: subjects.getAllSubjects,
-  removeSubject: subjects.removeSubject,
-  getLearning: learning.getLearning,
-  learningSubject: learning.learningSubject,
-  findSubjectsByUser: learning.findSubjectsByUser,
-  removeSubjectByUser: learning.removeSubjectByUser,
-  teachingSubject: teaching.teachingSubject,
-  getSubjectForTeacher: teaching.getSubjectForTeacher,
-  getTeaching: teaching.getTeaching,
-  insertTeacher: teaching.insertTeacher,
-  addAppointment: sessions.addAppointment,
-  addSessionsToUsers: sessions.addSessionsToUsers,
-  getAppointmentsByUser: sessions.getAppointmentsByUser,
-  getAppointmentTutor: sessions.getAppointmentTutor,
-  getTeachersInfo: teachers.getTeachersInfo,
-  getAllTeachers: teachers.getAllTeachers,
-  setAvailability: available.setAvailability,
-  advancedSearch: advSearch.advancedSearch
-};
-
+module.exports = _.assign({},
+  users,
+  subjects,
+  available,
+  learning,
+  teaching,
+  sessions,
+  teachers,
+  advSearch
+);
