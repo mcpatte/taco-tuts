80dcac7ad3ccefa2ae4fb6cea24cd9e55be307ad
graftss <coughcoughcough@gmail.com>
2016-08-16 19:59:51 -0400

    (feat) add `SessionRequestActions` methods

--- src/client/actions/session-request.actions.ts
+++ src/client/actions/session-request.actions.ts
@@ -2,9 +2,11 @@ import { NgRedux } from 'ng2-redux';
 import { IAppState } from '../store';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import 'rxjs/add/operator/do';
 
 export const SESSION_REQUEST_ACTIONS = {
-  SET_REQUESTS: 'SET_REQUESTS'
+  SET_REQUESTS: 'SET_REQUESTS',
+  ADD_REQUEST: 'ADD_SESSION_REQUEST'
 };
 
 @Injectable()
@@ -14,7 +16,28 @@ export class SessionRequestActions {
     private http: Http
   ) {}
 
+  addRequestDispatch(studentAuthID, teacherAuthID, subjectID) {
+    const body = { studentAuthID, teacherAuthID, subjectID };
+
+    return this.http.post('/api/instantsessions/', body)
+      .do(this.syncStudentRequestsDispatch);
+  }
+
+  addRequest(request) {
+    return {
+      type: SESSION_REQUEST_ACTIONS.ADD_REQUEST,
+      request
+    };
+  }
+
+  syncStudentRequestsDispatch() {
+    const authid = this.ngRedux.getState().login.userData.authid;
+    return this.http.get('/api/instantsessions/student/' + authid)
+      .do(requests => this.setRequestsDispatch(requests));
+  }
+
   setRequestsDispatch(requests) {
+    console.log('got requests', { requests });
     this.ngRedux.dispatch(this.setRequests(requests));
   }
 
