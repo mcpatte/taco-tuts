308e8f09e8319af4010fbd7546db145d03eec125
Emily Jones <efhjones@gmail.com>
2016-08-16 00:08:33 -0600

    (feat) set up template and advanced-search component to collect user query and send to service

--- src/client/components/advanced-search/advanced-search.component.ts
+++ src/client/components/advanced-search/advanced-search.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
-import { NgRedux } from 'ng2-redux';
-import { IAppState } from '../../store/index';
+import { Component }              from '@angular/core';
+import { NgRedux }                from 'ng2-redux';
+import { IAppState }              from '../../store/index';
+import { AdvancedSearchService }  from '../../services/advanced-search.service';
 
 @Component({
   selector: 'advanced-search',
-  providers: [ ],
+  providers: [ AdvancedSearchService ],
   template: require('./advanced-search.template.html')
 })
 export class AdvancedSearchComponent {
+
+  private userParams: Object = {
+    name: null,
+    subject: null,
+    rating: null,
+    currentlyAvailable: null
+  };
+
   constructor(
-    private ngRedux: NgRedux<IAppState>
+    private ngRedux: NgRedux<IAppState>,
+    private advSearch: AdvancedSearchService
     ) { }
+    
+  search(userParams: Object) {
+    console.log('search clicked with ', userParams);
+    this.advSearch.advancedSearch(userParams)
+
+  }
+
+  get diagnostic() { return JSON.stringify(this.userParams); }   
 }
 
