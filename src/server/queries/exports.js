6735e6ebcc837ce3e682097731bee08127dcbaeb
kpl39 <kylelinhardt@gmail.com>
2016-08-17 14:50:10 -0400

    (feat) add levelUp method for progress bar

--- src/server/queries/exports.js
+++ src/server/queries/exports.js
@@ -22,36 +22,3 @@ module.exports = _.assign({},
   advSearch,
   instantSessions
 );
-
-// module.exports = {
-//   getAllUsers: users.getAllUsers,
-//   getSingleUser: users.getSingleUser,
-//   createUser: users.createUser,
-//   updateUser: users.updateUser,
-//   removeUser: users.removeUser,
-//   setAuthID: users.setAuthID,
-//   getTeachersForSubject: subjects.getTeachersForSubject,
-//   createSubject: subjects.createSubject,
-//   getAllSubjects: subjects.getAllSubjects,
-//   removeSubject: subjects.removeSubject,
-//   getLearning: learning.getLearning,
-//   learningSubject: learning.learningSubject,
-//   findSubjectsByUser: learning.findSubjectsByUser,
-//   removeSubjectByUser: learning.removeSubjectByUser,
-//   teachingSubject: teaching.teachingSubject,
-//   getSubjectForTeacher: teaching.getSubjectForTeacher,
-//   getTeaching: teaching.getTeaching,
-//   insertTeacher: teaching.insertTeacher,
-//   addAppointment: sessions.addAppointment,
-//   addSessionsToUsers: sessions.addSessionsToUsers,
-//   getAppointmentsByUser: sessions.getAppointmentsByUser,
-//   getAppointmentTutor: sessions.getAppointmentTutor,
-//   getTeachersInfo: teachers.getTeachersInfo,
-//   getAllTeachers: teachers.getAllTeachers,
-//   setAvailability: available.setAvailability,
-//   confirmAppt: sessions.confirmAppt,
-//   removeAppt: sessions.removeAppt,
-//   levelUp: learning.levelUp
-// };
-
-
