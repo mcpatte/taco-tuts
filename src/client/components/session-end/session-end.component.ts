42467e2c936a6b84ae6e1e6b352aa63b970dc06f
graftss <coughcoughcough@gmail.com>
2016-08-18 13:37:32 -0400

    (feat) scaffold `SessionEndComponent` template

--- src/client/components/session-end/session-end.component.ts
+++ src/client/components/session-end/session-end.component.ts
@@ -3,8 +3,22 @@ import { IAppState } from '../../store';
 
 @Component({
   selector: 'session-end',
-  template: require('./session-end.template.html')
+  template: require('./session-end.template.html'),
+  styles: [`
+    .rating {
+      padding: 0 5px 0 5px;
+    }
+
+    .review {
+      width: 20%;
+      height: 10%;
+    }
+  `]
 })
 export class SessionEndComponent {
-  constructor() { }
+  constructor() {}
+
+  submitReview() {
+    console.log('submitting review');
+  }
 }
