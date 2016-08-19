6d4055de04b014ae2ccbf86a5bcb93e3426c708b
Chase Patterson <mcpatte@gmail.com>
2016-08-19 11:05:04 -0700

    (fix) put profilepics in the correct location and add some style fixes to the home page

--- src/client/components/home/teacherSearch.component.ts
+++ src/client/components/home/teacherSearch.component.ts
@@ -14,20 +14,36 @@ import { TeacherList } from '../../actions/teacherList.actions';
     providers: [ AppointmentService, StudentDashboardService, AdvancedSearchService, TeacherList],
     styles: [`
         .filter-input: {
-          color: blue;
+          
+        }
+        input[type=text] {
+          width: 100%;
+          box-sizing: border-box;
+          border: 2px solid #ccc;
+          border-radius: 4px;
+          font-size: 16px;
+          background-color: white;
+          background-position: 10px 10px;
+          background-repeat: no-repeat;
+          padding: 12px 5px 10px 10px;
+          -webkit-transition: width 0.4s ease-in-out;
+          transition: width 0.4s ease-in-out;
+        }
+        div {
+          margin: 0;
+          padding: 0;
+        }
+
+        input[type=text]:focus {
+            width: 200%;
         }
     `],
     template: `
-        <div class="container" >
-            <div class="input-field col s12">
-              <input id="subject" type="text" class="validate filter-input" placeholder="What do you want to learn?" [(ngModel)]=query (keyup)=debounce() size="35">
-              
-            </div>
-            <div class="suggestions" *ngIf="filteredList.length > 0">
-                <div *ngFor="#item of filteredList" >
-                        <button pButton (click)="select(item)" label={{item}}> </button>
-                </div>
-            </div>
+        <br />
+        <div class="container-fluid" >
+
+            <input id="subject" type="text" class="validate filter-input" placeholder="Filter teachers by subject..." [(ngModel)]=query (keyup)=debounce()>
+
         </div>  	
         `
 })
