13197adc775b92809db6704c4c85a4158d631b5b
Emily Jones <efhjones@gmail.com>
2016-08-18 23:37:49 -0600

    (chore) remove unused imports

--- src/client/containers/app.component.ts
+++ src/client/containers/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 import { Router, ROUTER_DIRECTIVES } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
 import { NgRedux, DevToolsExtension } from 'ng2-redux';
-import { AdvancedSearchComponent } from '../components/advanced-search';
-import { SearchBarComponent } from '../components/searchBar.component';
-import { StudentDashboardComponent } from '../components/student-dashboard/studentDashboard.component';
 import { MenuBarComponent } from '../components/menuBar.component';
 import { IAppState } from '../store/index';
 import { ConfigureStoreService } from '../services/configure-store.service.ts';
@@ -46,40 +43,19 @@ export class AppComponent {
   ) {
     configureStore.configure(ngRedux);
 
-    auth.userData$.subscribe(this.onNewUserData.bind(this))
+    auth.userData$.subscribe(this.onNewUserData.bind(this));
   }
 
   onNewUserData(userData) {
     this.loginActions.setDataDispatch(userData);
 
     this.socket.connect(userData.authid);
-
-    if (userData.teacher) {
-      this.teacherSocket.init();
-    }
   }
-
-  ngOnInit() {
-    // this.setProfile();
-  }
-
-  setProfile() {
-    if (this.auth.isAuthenticated()) {
-      // this.initialized = true;
-      if (this.getID().indexOf('google') !== -1) {
-         this.auth.fetchAuth0Profile(this.getID(), function(profile) {
-           this.auth.setProfile(this.getID(), profile);
-         });
-      }
-    }
-  }
-
-
 
   getID() {
     let currState = this.ngRedux.getState();
     if (currState.login.userData !== null) {
-        return currState.login['userData'].authid;
+      return currState.login['userData'].authid;
     }
     return undefined;
   }
