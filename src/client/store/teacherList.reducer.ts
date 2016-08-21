30216a3c3e0e08f807bd1c2f4e90d6e9c8d0bcc1
Emily Jones <efhjones@gmail.com>
2016-08-20 22:27:02 -0600

    (chore) add initial state for development (to be removed)

--- src/client/store/teacherList.reducer.ts
+++ src/client/store/teacherList.reducer.ts
@@ -5,7 +5,15 @@ export interface ITeacherListState {
 }
 
 const INIT_STATE = {
-  list: []
+  list: [],
+  selectedTeacher: JSON.parse(
+    `{"id":5,"authid":"auth0|57b2801371c16ce874b94fdd",
+    "username":"Headmaster","name":"Dumbledore",
+    "imageurl":"http://i.imgur.com/uJzJ4ix.jpg",
+    "email":"ddore@gmail.com","teacherid":5,"teacher":true,"isavailable":true,
+    "favorite":false,"rating":5,"ratingcount":0,"rate":null,"summary":null,
+    "subjects":["Taco Making", "Dueling"]}`
+  )
 };
 
 export function teacherListReducer(
@@ -16,6 +24,10 @@ export function teacherListReducer(
     case TEACHER_LIST_ACTIONS.SET_LIST:
       return Object.assign({}, state, {
         list: action.list
+      });
+    case TEACHER_LIST_ACTIONS.SET_TEACHER:
+      return Object.assign({}, state, {
+        selectedTeacher: action.teacher
       });
     default:
       return state;
