63d5e0e9b13a4a6e039b88bc6c110baf098d7148
graftss <coughcoughcough@gmail.com>
2016-08-24 19:45:14 -0400

    (fix) clear `session` state after logout

--- src/client/actions/index.ts
+++ src/client/actions/index.ts
@@ -1,5 +1,6 @@
 export * from './teacher.actions';
 export * from './session.actions';
 export * from './login.actions';
+export * from './logout.actions';
 export * from './session-request.actions';
 export * from './teacherList.actions';
