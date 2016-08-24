826e632fb923d54ee4936fb4fd82901c928d9afb
kpl39 <kylelinhardt@gmail.com>
2016-08-24 16:11:55 -0400

    (feat) add method to get teacher information

--- src/client/services/profile.service.ts
+++ src/client/services/profile.service.ts
@@ -1,5 +1,8 @@
-import { Injectable }     from '@angular/core';
-import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class ProfileService {
@@ -12,4 +15,25 @@ export class ProfileService {
   updateRate(userData){ 
     return this.http.put('/api/teachers/rate', userData)
   }
+
+  getTeacherInfo(userID) {
+    return this.http.get('/api/teachers/' + userID)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+   private extractData(res: Response) {
+    let body = res.json();
+    console.log('BODY', body.data);
+    return body.data || { };
+  }
+
+  private handleError (error: any) {
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+
+
 }
