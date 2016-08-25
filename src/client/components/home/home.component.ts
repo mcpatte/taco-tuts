cc90f4b42ec4707b33d2c5d914e6c4dbf8eb466b
Emily Jones <efhjones@gmail.com>
2016-08-24 22:04:03 -0600

    (style) make styling consistent between request session and view profile buttons

--- src/client/components/home/home.component.ts
+++ src/client/components/home/home.component.ts
@@ -57,9 +57,9 @@ import { TeacherListActions } from '../../actions';
       margin-top: 10px;
     }
     .btn-session:active {
-      background-color: #33495f;
+      background-color: #ff9f4f;
       font-weight: 800;
-      color: #ff9f4f;
+      color: #33495f;
       border: 0;
     }
     .btn-session-cancel {
